a051b068a20e620289d5e7a6300c85db10abfdfb
Kent Fredric <kentfredric@gmail.com>
2014-01-31 17:28:36 +1300

    Build results of e187c7d (on master)

--- lib/Dist/Zilla/Plugin/Git/NextRelease.pm
+++ lib/Dist/Zilla/Plugin/Git/NextRelease.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 
 package Dist::Zilla::Plugin::Git::NextRelease;
-$Dist::Zilla::Plugin::Git::NextRelease::VERSION = '0.002001';
+$Dist::Zilla::Plugin::Git::NextRelease::VERSION = '0.002010';
 # ABSTRACT: Use time-stamp from Git instead of process start time.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -178,7 +178,7 @@ Dist::Zilla::Plugin::Git::NextRelease - Use time-stamp from Git instead of proce
 
 =head1 VERSION
 
-version 0.002001
+version 0.002010
 
 =head1 SYNOPSIS
 
